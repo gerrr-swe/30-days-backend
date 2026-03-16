bda9288c11dd8af19a3e9c61dbef308b97eb12d8
gerrr-swe <gerardo.ramirez.swe@gmail.com>
2026-03-16 14:37:14 -0600

    feat: add websocket listener in Server

--- 01-simple-server/src/main/java/Server.java
+++ 01-simple-server/src/main/java/Server.java
@@ -1,14 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
 
     private static final int PORT = 8080;
+    private static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+    private static final String dynamic_path = "/dynamic.html";
+    private static final byte[] NOT_FOUND = "<h1>404: Content not found</h1>".getBytes();
 
     public static void main(String[] args) {
         try (ServerSocket socket = new ServerSocket(PORT)) {
@@ -30,5 +36,23 @@ public class Server {
             line = reader.readLine();
             requestLines.add(line);
         } while (!line.isBlank());
+
+        // GET /index.html HTTP/1.1
+        String[] requestLine = requestLines.getFirst().split(" ");
+        String path = requestLine[1];
+
+        Path filePath = Paths.get(".",path);
+
+        if (dynamic_path.equals(path)) {
+
+        }
+
+    }
+
+    private static void sendResponse(Socket client, String status, String contentType, byte[] content) throws IOException {
+        String lineBreak = "\r\n";
+        OutputStream output = client.getOutputStream();
+        output.write(("HTTP/1.1 "+status).getBytes());
+        output.write(("ContentType: " + contentType + lineBreak).getBytes());
     }
 }
